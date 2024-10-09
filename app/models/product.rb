669a502e08477733d39dfbe81a47fd12bc4d4672
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-10-09 10:20:10 +0200

    Alcune modifiche apportate prima di inizare con la sezione 5

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3, maximum: 10 }
-  validates :description, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 300 }
   validates :price, presence: true, numericality: true
 end
