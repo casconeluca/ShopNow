7ced9742c038f63b6e232a5f7033d7f160af12ec
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-10-09 09:54:49 +0200

    Allineato alla fine della sezione 4

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 10 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :price, presence: true, numericality: true
 end
