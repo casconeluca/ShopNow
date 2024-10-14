be34ba73eea144a7621fe18657ac409e2d50edc1
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-10-14 10:26:28 +0200

    Aggiunta la tabella utenti e il modello utenti

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_09_070959) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_14_081440) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
