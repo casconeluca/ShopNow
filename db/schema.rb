13ec5267f23ccdb5776a33cb375b34fba1967691
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-10-14 15:35:46 +0200

    Installata la gemma Bcrypt e aggiunta la colonna password_digest nella tabella degli utenti per la memorizzazione della password criptata

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_14_093510) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_14_131539) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -25,5 +25,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_14_093510) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 end
