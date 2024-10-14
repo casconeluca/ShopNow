15b923bd07c6d7460398f691b0a2bb4a19e5cbc7
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-10-14 12:22:12 +0200

    Aggiunta  associazione 1 a molti tra la tabella Products e User

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_14_081440) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_14_093510) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
