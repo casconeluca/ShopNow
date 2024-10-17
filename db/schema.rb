ad6f813ecdf618f9d818ea7f4775a4ad1934265a
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-10-17 11:56:33 +0200

    Aggiunta una colonna admin alla tabella users di valori booleani per verificare se un utente è admin o no

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_14_131539) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_17_095201) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -26,5 +26,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_14_131539) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 end
