a7d3f1fa2a89caeacdf6dfc912f32f35ee0441b0
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-10-21 10:07:18 +0200

    Creato il modello e la tabella per le categorie e creato il file per i test per le categorie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_17_095201) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_21_073932) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
