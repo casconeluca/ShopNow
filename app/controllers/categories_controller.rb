4d7507bae63d8adc52f849daa3c8ba9f7019e004
LUCA CASCONE <luca.cascone@digitalpivot.it>
2024-10-22 16:34:34 +0200

    Aggiunta la show view per le categorie

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,6 +21,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @products = @category.products.paginate(page: params[:page], per_page: 3)
   end
 
   private
